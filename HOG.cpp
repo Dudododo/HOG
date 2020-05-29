f46a7daa4a487011a81d80124ba1e76ef36d343f
Dudododo <a752601260@gmail.com>
2020-05-29 14:45:12 +0800

    寻找目标物体

--- HOG.cpp
+++ HOG.cpp
@@ -12,7 +12,7 @@ float *cellHist(Mat src)
 	Mat mag, angle;
 	Sobel(src, gx, CV_32F, 1, 0, 1);
 	Sobel(src, gy, CV_32F, 0, 1, 1);
-	cartToPolar(gx, gy, mag, angle, false);
+	cartToPolar(gx, gy, mag, angle, true);
 
 	//cell直方图数组
 	float hist[8] = { 0 };  
@@ -62,94 +62,90 @@ float *cellHist(Mat src)
 
 int main()
 {
-	Mat refMat, img1, img2;
-	refMat = imread("hogTemplate.jpg", 0);
-	img1 = imread("img1.jpg", 0);
-	img2 = imread("img2.jpg", 0);
-		 
+	Mat refMat = imread("template.png", 0);
+	Mat srcMat = imread("img.png", 0);
 	int cellSize = 16;
 	int nX = refMat.cols / cellSize;
 	int nY = refMat.rows / cellSize;
+	Mat img = Mat::zeros(refMat.rows, refMat.cols, CV_8UC1);
 	int num = nX * nY * 8;
-	float * refHist = new float[num];
-	memset(refHist, 0, sizeof(float) * num);
-	float * img1Hist = new float[num];
-	memset(img1Hist, 0, sizeof(float) * num);
-	float * img2Hist = new float[num];
-	memset(img2Hist, 0, sizeof(float) * num);
 
-	//refMat的直方图数组
-	int cnt = 0;
-	for (int j = 0; j < nY; j++)
-	{
-		for (int i = 0; i < nX; i++)
-		{
-			Rect rectL(i*16, j*16, 16, 16);  //分割图像
-			Mat nowMat = refMat(rectL);
-			for (int k = 0; k < 8; k++)  //合并直方图数组
-			{
-				refHist[k + cnt * 8] = cellHist(nowMat)[k];
-			}
-			cnt++;
-		}
-	}
+	float* srcHist = new float[num];
+	memset(srcHist, 0, sizeof(float) * num);
 
-	//img1的直方图数组
+	float* refMatHist = new float[num];
+	memset(refMatHist, 0, sizeof(float) * num);
+
+	float* imgHist = new float[num];
+	memset(imgHist, 0, sizeof(float) * num);
+
 	int cnt1 = 0;
-	for (int j = 0; j < nY; j++)
-	{
-		for (int i = 0; i < nX; i++)
-		{
-			Rect rectL(i * 16, j * 16, 16, 16);  //分割图像
-			Mat nowMat = img1(rectL);
-			for (int k = 0; k < 8; k++)  //合并直方图数组
-			{
-				img1Hist[k + cnt1 * 8] = cellHist(nowMat)[k];
-			}
-			cnt1++;
-		}
-	}
-
-	//img2的直方图数组
 	int cnt2 = 0;
-	for (int j = 0; j < nY; j++)
+	float distance = 0;
+	float distance_min = 10e100;
+	int x = 0;
+	int y = 0;
+	for (int q = 80; q < /*srcMat.rows- refMat.rows*/110; q++)
 	{
-		for (int i = 0; i < nX; i++)
+		for (int p = 80; p < /*srcMat.cols - refMat.cols*/110; p++)
 		{
-			Rect rectL(i * 16, j * 16, 16, 16);  //分割图像
-			Mat nowMat = img2(rectL);
-			for (int k = 0; k < 8; k++)  //合并直方图数组
+			for (int m = 0; m < refMat.rows; m++) 
 			{
-				img2Hist[k + cnt2 * 8] = cellHist(nowMat)[k];
+				for (int n = 0; n < refMat.cols; n++)
+				{
+					img.at<uchar>(m, n) = srcMat.at<uchar>(q + m, p + n);
+				}
 			}
-			cnt2++;
+			//refMat
+			cnt1 = 0;
+			for (int j = 0; j < nY; j++)
+			{
+				for (int i = 0; i < nX; i++)
+				{
+					Rect rectL(i * cellSize, j * cellSize, cellSize, cellSize);
+					Mat nowMat = refMat(rectL);
+					for (int k = 0; k < 8; k++)
+					{
+						refMatHist[k + cnt1 * 8] = cellHist(nowMat)[k];
+					}
+					cnt1++;
+				}
+			}
+			//img
+			cnt2 = 0;
+			for (int j = 0; j < nY; j++)
+			{
+				for (int i = 0; i < nX; i++)
+				{
+					Rect rectL(i * cellSize, j * cellSize, cellSize, cellSize);
+					Mat nowMat = img(rectL);
+					for (int k = 0; k < 8; k++)
+					{
+						imgHist[k + cnt2 * 8] = cellHist(nowMat)[k];
+					}
+					cnt2++;
+				}
+			}
+			distance = 0;
+			for (int i = 0; i < num; i++)
+			{
+				distance += (refMatHist[i] - imgHist[i]) * (refMatHist[i] - imgHist[i]);
+				if (distance < distance_min) {
+					distance_min = distance;
+					x = q;
+					y = p;
+				}
+			}
 		}
 	}
 
-	//计算欧几里得距离
-	float distance1, distance2;
-	float sum1 = 0, sum2 = 0;
-	for (int i = 0; i < num; i++)
-	{
-		sum1 += (refHist[i] - img1Hist[i]) * (refHist[i] - img1Hist[i]);
-		sum2 += (refHist[i] - img2Hist[i]) * (refHist[i] - img2Hist[i]);
-	}
-	distance1 = sqrt(sum1);
-	distance2 = sqrt(sum2);
-	cout << "img1与参考图的欧几里得距离：" << distance1 << endl;
-	cout << "img2与参考图的欧几里得距离：" << distance2 << endl;
-	if (distance1 < distance2)
-	{
-		cout << "img1与参考图更为接近" << endl;
-	}
-	else
-	{
-		cout << "img2与参考图更为接近" << endl;
-	}
-	delete[] refHist;
-	delete[] img1Hist;
-	delete[] img2Hist;
-
+	//框出目标物体
+	Rect rect(y, x, refMat.cols, refMat.rows);
+	cv::rectangle(srcMat, rect, Scalar(255, 0, 0), 1, LINE_8, 0);
+	imshow("srcMat", srcMat);
+	waitKey(0);
+	delete[] refMatHist;
+	delete[] imgHist;
 	return 0;
-}
 
+}
